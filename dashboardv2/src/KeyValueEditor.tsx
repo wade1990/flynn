21f06779105c72527c154dfad026d8cb473db60c
Jesse Stuart <email@jessestuart.ca>
2019-03-13 08:29:58 -0400

    dashboardv2: Refactor EnvEditor into KeyValueEditor
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

---
+++ dashboardv2/src/KeyValueEditor.tsx
@@ -0,0 +1,357 @@
+import * as React from 'react';
+import * as jspb from 'google-protobuf';
+import fz from 'fz';
+
+import Button from 'grommet/components/Button';
+import { CheckmarkIcon, SearchInput } from 'grommet';
+import { Diff, DiffOp } from './util/protoMapDiff';
+
+import './KeyValueEditor.scss';
+
+export type Entries = jspb.Map<string, string>;
+
+function entriesEqual(a: [string, string], b: [string, string]): boolean {
+	return a && b && a[0] === b[0] && a[1] === b[1];
+}
+
+interface KeyValueDataInternalState {
+	originalEntries: Entries;
+	uniqueKeyMap: { [key: string]: number };
+	filterText: string;
+	deletedIndices: { [key: number]: boolean };
+	changedIndices: { [key: number]: boolean };
+}
+
+export class KeyValueData {
+	public length: number;
+	public deletedLength: number;
+	public hasChanges: boolean;
+	private _entries: Entries;
+	private _state: KeyValueDataInternalState;
+	constructor(entries: Entries, state: KeyValueDataInternalState | null = null) {
+		this._entries = entries;
+		this._state = state
+			? state
+			: {
+					originalEntries: entries,
+					changedIndices: {},
+					deletedIndices: {},
+					uniqueKeyMap: entries
+						.toArray()
+						.reduce((m: { [key: string]: number }, [key, value]: [string, string], index: number) => {
+							m[key] = index;
+							return m;
+						}, {}),
+					filterText: ''
+			  };
+		this.length = entries.getLength();
+		this._setDeletedLength();
+	}
+
+	public dup(): KeyValueData {
+		return new KeyValueData(this._entries, Object.assign({}, this._state));
+	}
+
+	public filtered(filterText: string): KeyValueData {
+		return new KeyValueData(this._entries, Object.assign({}, this._state, { filterText }));
+	}
+
+	public get(key: string): string | undefined {
+		return this._entries.get(key);
+	}
+
+	public entries(): Entries {
+		return new jspb.Map(
+			this._entries.toArray().filter(
+				(entry: [string, string], index: number): boolean => {
+					return this._state.deletedIndices[index] !== true && entry[0] !== '' && entry[1] !== '';
+				}
+			)
+		);
+	}
+
+	public map<T>(fn: (entry: [string, string], index: number) => T): T[] {
+		const filterText = this._state.filterText;
+		return (
+			this._entries
+				.toArray()
+				.reduce<T[]>(
+					(prev: T[], entry: [string, string], index: number): T[] => {
+						if (this._state.deletedIndices[index] === true) {
+							return prev;
+						}
+						if (filterText && !fz(entry[0], filterText)) {
+							return prev;
+						}
+						return prev.concat(fn(entry, index));
+					},
+					[] as Array<T>
+				)
+				// there's always an empty entry at the end for adding new env
+				.concat(fn(['', ''], this.length))
+		);
+	}
+
+	public mapDeleted<T>(fn: (entry: [string, string], index: number) => T): T[] {
+		return this._entries.toArray().reduce<T[]>(
+			(prev: T[], entry: [string, string], index: number): T[] => {
+				if (this._state.deletedIndices[index] !== true) {
+					return prev;
+				}
+				return prev.concat(fn(entry, index));
+			},
+			[] as Array<T>
+		);
+	}
+
+	public applyDiff(diff: Diff<string, string>) {
+		diff.forEach((op: DiffOp<string, string>) => {
+			let index = this._entries.toArray().findIndex(([key, value]: [string, string]) => {
+				return key === op.key;
+			});
+			switch (op.op) {
+				case 'add':
+					if (op.value) {
+						if (index === -1) {
+							index = this._entries.toArray().length;
+						}
+						this.setKeyAtIndex(index, op.key);
+						this.setValueAtIndex(index, op.value);
+					}
+					break;
+				case 'remove':
+					if (index !== -1) {
+						this.removeEntryAtIndex(index);
+					}
+					break;
+			}
+		});
+	}
+
+	public setKeyAtIndex(index: number, key: string) {
+		delete this._state.deletedIndices[index]; // allow restoring an item
+		this._setDeletedLength();
+		const entries = this._entries.toArray().slice(); // don't modify old map
+		entries[index] = [key, (entries[index] || [])[1] || ''];
+		this.length = entries.length;
+		this._entries = new jspb.Map(entries);
+		this._trackChanges(index);
+		if (this._state.uniqueKeyMap[key] > -1 && this._state.uniqueKeyMap[key] !== index && index < entries.length) {
+			// duplicate key, remove old one
+			this.removeEntryAtIndex(this._state.uniqueKeyMap[key]);
+			this._state.uniqueKeyMap[key] = index;
+		}
+	}
+
+	public setValueAtIndex(index: number, val: string) {
+		const entries = this._entries.toArray().slice(); // don't modify old map
+		entries[index] = [(entries[index] || [])[0] || '', val];
+		this.length = entries.length;
+		this._entries = new jspb.Map(entries);
+		if (val === '' && (entries[index] || [])[0] === '') {
+			// if there's no key or value, remove it
+			this.removeEntryAtIndex(index);
+		} else {
+			this._trackChanges(index);
+		}
+	}
+
+	public removeEntryAtIndex(index: number) {
+		this._state.deletedIndices[index] = true;
+		this._setDeletedLength();
+		this._trackChanges(index);
+	}
+
+	private _trackChanges(index: number) {
+		const { deletedIndices, changedIndices, originalEntries } = this._state;
+		if (deletedIndices[index] === true) {
+			if (index < originalEntries.getLength()) {
+				changedIndices[index] = true;
+			} else {
+				delete changedIndices[index];
+			}
+		} else if (entriesEqual(originalEntries.toArray()[index], this._entries.toArray()[index])) {
+			delete changedIndices[index];
+		} else {
+			changedIndices[index] = true;
+		}
+		this.hasChanges = Object.keys(changedIndices).length > 0;
+	}
+
+	private _setDeletedLength() {
+		this.deletedLength = Object.keys(this._state.deletedIndices).length;
+	}
+}
+
+interface KeyValueInputProps {
+	placeholder: string;
+	value: string;
+	onChange: (value: string) => void;
+	disabled?: boolean;
+}
+
+interface KeyValueInputState {
+	expanded: boolean;
+	multiline: boolean;
+}
+
+class KeyValueInput extends React.Component<KeyValueInputProps, KeyValueInputState> {
+	private _textarea: HTMLTextAreaElement | null;
+
+	constructor(props: KeyValueInputProps) {
+		super(props);
+		this.state = {
+			expanded: false,
+			multiline: props.value.indexOf('\n') >= 0
+		};
+		this._inputChangeHandler = this._inputChangeHandler.bind(this);
+		this._inputFocusHandler = this._inputFocusHandler.bind(this);
+		this._textareaBlurHandler = this._textareaBlurHandler.bind(this);
+		this._textareaChangeHandler = this._textareaChangeHandler.bind(this);
+		this._textarea = null;
+	}
+
+	public componentDidUpdate(prevProps: KeyValueInputProps, prevState: KeyValueInputState) {
+		if (!prevState.expanded && this.state.expanded && this._textarea) {
+			this._textarea.focus();
+		}
+	}
+
+	public render() {
+		const { placeholder, value, disabled } = this.props;
+		const { expanded } = this.state;
+		if (expanded) {
+			return (
+				<textarea
+					value={value}
+					onChange={this._textareaChangeHandler}
+					onBlur={this._textareaBlurHandler}
+					ref={(el) => {
+						this._textarea = el;
+					}}
+				/>
+			);
+		}
+		return (
+			<input
+				type="text"
+				disabled={disabled}
+				placeholder={placeholder}
+				value={value}
+				onChange={this._inputChangeHandler}
+				onFocus={this._inputFocusHandler}
+			/>
+		);
+	}
+
+	private _inputChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+		const value = e.target.value || '';
+		this.props.onChange(value);
+	}
+
+	private _textareaChangeHandler(e: React.ChangeEvent<HTMLTextAreaElement>) {
+		const value = e.target.value || '';
+		this.props.onChange(value);
+	}
+
+	private _inputFocusHandler() {
+		if (this.state.multiline) {
+			this.setState({
+				expanded: true
+			});
+		}
+	}
+
+	private _textareaBlurHandler() {
+		if (this.state.expanded) {
+			this.setState({
+				expanded: false
+			});
+		}
+	}
+}
+
+export interface Props {
+	data: KeyValueData;
+	onChange: (data: KeyValueData) => void;
+	onSubmit: (data: KeyValueData) => void;
+	keyPlaceholder: string;
+	valuePlaceholder: string;
+	submitLabel: string;
+}
+
+interface State {}
+
+export default class KeyValueEditor extends React.Component<Props, State> {
+	public static defaultProps = {
+		keyPlaceholder: 'Key',
+		valuePlaceholder: 'Value',
+		submitLabel: 'Review Changes'
+	};
+
+	constructor(props: Props) {
+		super(props);
+		this.state = {};
+		this._searchInputHandler = this._searchInputHandler.bind(this);
+		this._submitHandler = this._submitHandler.bind(this);
+	}
+
+	public render() {
+		const { data, keyPlaceholder, valuePlaceholder, submitLabel } = this.props;
+
+		return (
+			<form onSubmit={this._submitHandler} className="kv-editor">
+				<SearchInput onDOMChange={this._searchInputHandler} />
+				{data.map(([key, value]: [string, string], index: number) => {
+					return (
+						<div key={index} className="kv-row">
+							<KeyValueInput
+								placeholder={keyPlaceholder}
+								value={key}
+								onChange={this._keyChangeHandler.bind(this, index)}
+							/>
+							<KeyValueInput
+								placeholder={valuePlaceholder}
+								value={value}
+								onChange={this._valueChangeHandler.bind(this, index)}
+							/>
+						</div>
+					);
+				})}
+				{data.hasChanges ? (
+					// Enable save button
+					<Button type="submit" primary icon={<CheckmarkIcon />} label={submitLabel} />
+				) : (
+					// Disable save button
+					<Button type="button" primary icon={<CheckmarkIcon />} label={submitLabel} />
+				)}
+			</form>
+		);
+	}
+
+	private _keyChangeHandler(index: number, key: string) {
+		let nextEntries = this.props.data.dup();
+		if (key.length > 0) {
+			nextEntries.setKeyAtIndex(index, key);
+		} else {
+			nextEntries.removeEntryAtIndex(index);
+		}
+		this.props.onChange(nextEntries);
+	}
+
+	private _valueChangeHandler(index: number, value: string) {
+		let nextEntries = this.props.data.dup();
+		nextEntries.setValueAtIndex(index, value);
+		this.props.onChange(nextEntries);
+	}
+
+	private _searchInputHandler(e: React.ChangeEvent<HTMLInputElement>) {
+		const value = e.target.value || '';
+		this.props.onChange(this.props.data.filtered(value));
+	}
+
+	private _submitHandler(e: React.SyntheticEvent) {
+		e.preventDefault();
+		this.props.onSubmit(this.props.data);
+	}
+}
