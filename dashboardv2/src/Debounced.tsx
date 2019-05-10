13b1170e170f4f61e9b9e7984a0a3cd766fb57a8
Jesse Stuart <email@jessestuart.ca>
2019-05-10 12:19:15 -0400

    dashboardv2: Refactor <Debounced> to use React hooks
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- dashboardv2/src/Debounced.tsx
+++ dashboardv2/src/Debounced.tsx
@@ -1,49 +1,23 @@
 import * as React from 'react';
-import { default as debounced, Func as DebouncedFunc } from './util/debounced';
+import { debounce } from 'lodash';
 
 export interface Props {
 	children: React.ReactNode;
 	timeoutMs?: number;
 }
 
-interface State {
-	shouldRender: boolean;
-}
+export default function Debounced({ children, timeoutMs = 0 }: Props) {
+	const [shouldRender, setShouldRender] = React.useState(false);
+	React.useEffect(
+		() => {
+			const { cancel } = debounce(() => setShouldRender(true), timeoutMs);
+			return cancel;
+		},
+		[timeoutMs]
+	);
 
-export default class Loading extends React.Component<Props, State> {
-	private _startRenderTimeout: DebouncedFunc;
-
-	constructor(props: Props) {
-		super(props);
-		this.state = {
-			shouldRender: false
-		};
-		this._startRenderTimeout = debounced(() => {
-			this.setState({
-				shouldRender: true
-			});
-		});
+	if (shouldRender) {
+		return <>children</>;
 	}
-
-	public componentDidMount() {
-		this._startRenderTimeout();
-	}
-
-	public componentDidUpdate() {
-		if (!this.state.shouldRender) {
-			this._startRenderTimeout();
-		}
-	}
-
-	public componentWillUnmount() {
-		this._startRenderTimeout.cancel();
-	}
-
-	public render() {
-		if (this.state.shouldRender) {
-			return this.props.children;
-		} else {
-			return null;
-		}
-	}
+	return null;
 }
