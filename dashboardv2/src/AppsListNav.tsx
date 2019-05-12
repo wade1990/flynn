30865533225787e5a3de3e9e77af393058c63240
Jesse Stuart <email@jessestuart.ca>
2019-05-12 12:14:46 -0400

    dashboardv2: Refactor AppsListNav to use react hooks
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- dashboardv2/src/AppsListNav.tsx
+++ dashboardv2/src/AppsListNav.tsx
@@ -1,118 +1,76 @@
 import * as React from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-
 import { Box } from 'grommet';
 
-import withClient, { ClientProps } from './withClient';
-import withErrorHandler, { ErrorHandlerProps } from './withErrorHandler';
+import useRouter from './useRouter';
+import useAppsList from './useAppsList';
+import { handleError } from './withErrorHandler';
+
 import Loading from './Loading';
 import NavLink from './NavLink';
-import { App } from './generated/controller_pb';
 import { parseURLParams, urlParamsToString } from './util/urlParams';
 
-export interface Props extends RouteComponentProps<{}>, ClientProps, ErrorHandlerProps {
+export interface Props {
 	onNav(path: string): void;
 }
 
-interface State {
-	isLoading: boolean;
-	apps: App[];
-}
-
-class AppsListNav extends React.Component<Props, State> {
-	private __listAppsStreamCancel: () => void;
-
-	constructor(props: Props) {
-		super(props);
-
-		this.state = {
-			isLoading: true,
-			apps: []
-		};
-
-		this.__listAppsStreamCancel = () => {};
-		this._navHandler = this._navHandler.bind(this);
-	}
-
-	public componentDidMount() {
-		this._getData();
-	}
-
-	public componentWillUnmount() {
-		this.__listAppsStreamCancel();
-	}
-
-	public render() {
-		const { location } = this.props;
-		const { isLoading, apps } = this.state;
-
-		if (isLoading) {
-			return <Loading />;
-		}
-
-		// some query params are persistent, make sure they're passed along if present
-		const params = parseURLParams(location.search, 'rhf', 's');
-		const search = urlParamsToString(params);
-
-		const appRoutes = apps.map((app, index) => {
-			const path = `/${app.getName()}`; // e.g. /apps/48a2d322-5cfe-4323-8823-4dad4528c090
-			return {
-				path,
-				search,
-				displayName: app.getDisplayName(), // e.g. controller
-				selected: location.pathname === path
-			};
-		});
-
-		return (
-			<Box tag="ul" margin="none" pad="none" flex={true} overflow="auto">
-				{appRoutes.map((r) => {
-					return (
-						<NavLink path={r.path} search={search} key={r.path} onNav={this._navHandler}>
-							<Box
-								tag="li"
-								direction="row"
-								justify="between"
-								align="center"
-								border="bottom"
-								pad={{ horizontal: 'medium', vertical: 'small' }}
-								basis="auto"
-								flex={false}
-								background={r.selected ? 'accent-1' : 'neutral-1'}
-							>
-								{r.displayName}
-							</Box>
-						</NavLink>
-					);
-				})}
-			</Box>
-		);
-	}
-
-	private _getData() {
-		const { client, handleError } = this.props;
-		this.setState({
-			isLoading: true
-		});
-		this.__listAppsStreamCancel();
-		this.__listAppsStreamCancel = client.listAppsStream((apps: App[], error: Error | null) => {
-			this.setState({
-				isLoading: false,
-				apps
-			});
-			if (error) {
-				handleError(error);
+export default function AppsListNav({ onNav }: Props) {
+	const { location } = useRouter();
+	const { apps, loading: isLoading, error: appsError } = useAppsList();
+	React.useEffect(
+		() => {
+			if (appsError) {
+				handleError(appsError);
 			}
-		});
+		},
+		[appsError]
+	);
+
+	if (isLoading) {
+		return <Loading />;
 	}
 
-	private _navHandler(path: string) {
-		const { location, onNav } = this.props;
+	// some query params are persistent, make sure they're passed along if present
+	const params = parseURLParams(location.search, 'rhf', 's');
+	const search = urlParamsToString(params);
+
+	const appRoutes = apps.map((app, index) => {
+		const path = `/${app.getName()}`; // e.g. /apps/48a2d322-5cfe-4323-8823-4dad4528c090
+		return {
+			path,
+			search,
+			displayName: app.getDisplayName(), // e.g. controller
+			selected: location.pathname === path
+		};
+	});
+
+	const navHandler = (path: string) => {
 		if (location.pathname === path) {
 			return;
 		}
 		onNav(path);
-	}
-}
+	};
 
-export default withErrorHandler(withClient(withRouter(AppsListNav)));
+	return (
+		<Box tag="ul" margin="none" pad="none" flex={true} overflow="auto">
+			{appRoutes.map((r) => {
+				return (
+					<NavLink path={r.path} search={search} key={r.path} onNav={navHandler}>
+						<Box
+							tag="li"
+							direction="row"
+							justify="between"
+							align="center"
+							border="bottom"
+							pad={{ horizontal: 'medium', vertical: 'small' }}
+							basis="auto"
+							flex={false}
+							background={r.selected ? 'accent-1' : 'neutral-1'}
+						>
+							{r.displayName}
+						</Box>
+					</NavLink>
+				);
+			})}
+		</Box>
+	);
+}
---
+++ dashboardv2/src/useAppsList.ts
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import useClient from './useClient';
+import { App } from './generated/controller_pb';
+
+export default function useAppsList() {
+	const client = useClient();
+	const [appLoading, setAppLoading] = React.useState(true);
+	const [apps, setApps] = React.useState<App[]>([]);
+	const [error, setError] = React.useState<Error | null>(null);
+	React.useEffect(
+		() => {
+			const cancel = client.listAppsStream((apps: App[], error: Error | null) => {
+				setAppLoading(false);
+				if (error) {
+					setError(error);
+					return;
+				}
+				setApps(apps);
+				setError(null);
+			});
+			return cancel;
+		},
+		[client]
+	);
+	return {
+		loading: appLoading,
+		apps,
+		error
+	};
+}
