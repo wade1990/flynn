13590a4751c3f03d78f3f8573f90441165b583a5
Jesse Stuart <email@jessestuart.ca>
2019-05-13 11:34:27 -0400

    dashboardv2: Refactor NavAnchor to use react hooks
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- dashboardv2/src/NavAnchor.tsx
+++ dashboardv2/src/NavAnchor.tsx
@@ -1,62 +1,49 @@
 import * as React from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { Anchor, AnchorProps } from 'grommet';
 import { Omit } from 'grommet/utils';
+import useRouter from './useRouter';
 
-export interface Props extends Omit<AnchorProps, 'onClick'>, RouteComponentProps<{}> {
+export interface Props extends Omit<AnchorProps, 'onClick'> {
 	path: string;
 	search?: string;
 	onClick?: (e: React.MouseEvent) => void;
 	onNav?: (path: string) => void;
+	children: React.ReactNode;
 }
 
-class NavAnchor extends React.Component<Props> {
-	constructor(props: Props) {
-		super(props);
-		this._onClick = this._onClick.bind(this);
-	}
+export default function NavAnchor({ path, search, onClick, onNav, children, ...anchorProps }: Props) {
+	const { history } = useRouter();
 
-	public render() {
-		const {
-			path,
-			search,
-			history,
+	return (
+		<Anchor
+			{...anchorProps}
+			href={history.createHref({ pathname: path, search })}
+			onClick={(e: React.MouseEvent) => {
+				if (onClick) {
+					onClick(e);
+				}
 
-			// filter out remaining Props
-			onNav,
+				if (e.defaultPrevented) {
+					// allow onClick prop to cancel this action via e.preventDefault()
+					return;
+				}
 
-			// filter out remaining RouteComponentProps
-			match,
-			location,
-			staticContext,
+				if (e.ctrlKey || e.metaKey || e.shiftKey || e.altKey) {
+					// don't do anything if any modifier key is pressed
+					// it's likely the user is trying to open the link in a new tab or window
+					return;
+				}
 
-			...anchorProps
-		} = this.props;
-		const href = history.createHref({ pathname: path, search });
-		return <Anchor {...anchorProps} href={href} onClick={this._onClick} />;
-	}
+				e.preventDefault();
 
-	private _onClick(e: React.MouseEvent) {
-		if (this.props.onClick) {
-			this.props.onClick(e);
-		}
-		if (e.defaultPrevented) {
-			// allow this.props.onClick to cancel this action via e.preventDefault()
-			return;
-		}
-		if (e.ctrlKey || e.metaKey || e.shiftKey || e.altKey) {
-			// don't do anything if any modifier key is pressed
-			// it's likely the user is trying to open the link in a new tab or window
-			return;
-		}
-		const { path, search, history } = this.props;
-		e.preventDefault();
-		// TODO(jvatic): Handle both path and search containing query params
-		history.push(path + search);
-		if (this.props.onNav) {
-			this.props.onNav(path);
-		}
-	}
+				// TODO(jvatic): Handle both path and search containing query params
+				history.push(path + search);
+				if (onNav) {
+					onNav(path);
+				}
+			}}
+		>
+			{children}
+		</Anchor>
+	);
 }
-
-export default withRouter(NavAnchor);
